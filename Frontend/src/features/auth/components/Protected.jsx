3232789b90fc7bd0daf5d84647bdccff68cf0638
yash <yxshcodes@gmail.com>
2026-07-06 06:43:51 +0530

    Changes commited

--- Frontend/src/features/auth/components/Protected.jsx
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -2,6 +2,7 @@ import { useAuth } from "../hooks/useAuth";
 import { Navigate } from "react-router";
 import React from 'react'
 
+
 const Protected = ({children}) => {
     const { loading,user } = useAuth()
 
